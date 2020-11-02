fc0d380cfebc7ce3e9767efc16ae62fe59c4ee93
Saron <saronnhong@gmail.com>
2020-11-02 15:01:30 -0800

    improvements to best time to buy stocks

--- Best Time To Buy Stocks/bestTimeToBuyAndSellStocks.js
+++ Best Time To Buy Stocks/bestTimeToBuyAndSellStocks.js
@@ -16,16 +16,37 @@
 // Output: 0
 // Explanation: In this case, no transaction is done, i.e. max profit = 0.
 
+
+// BRUTE FORCE
+// Time Complexity = O(n^2)
+// Space Complexity = O(1)
+// var maxProfit = function(prices) {
+//     let max = 0;
+//     let curr_max = 0;
+//     for(let i = 0; i < prices.length; i++){
+//         for(let j = i+1; j < prices.length; j++){
+//             curr_max = prices[j] - prices[i];
+//             if(curr_max > max){
+//                 max = curr_max;
+//             }
+//         }
+//     }
+//     return max;
+// };
+
+//ONE PASS
+//Time Complexity = O(n)
+//Space Complexity = O(1)
 var maxProfit = function(prices) {
-    let max = 0;
-    let curr_max = 0;
+    let max_price = 0;
+    let min_price = Number.POSITIVE_INFINITY;
     for(let i = 0; i < prices.length; i++){
-        for(let j = i+1; j < prices.length; j++){
-            curr_max = prices[j] - prices[i];
-            if(curr_max > max){
-                max = curr_max;
-            }
+        if(prices[i] < min_price){
+            min_price = prices[i];
+        }
+        if(prices[i] - min_price > max_price){
+            max_price = prices[i] - min_price;
         }
     }
-    return max;
+    return max_price;
 };
