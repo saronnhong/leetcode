7c2b178e22e8cfe8c37ac0a5f65453ce6c5b234a
Saron <saronnhong@gmail.com>
2020-11-30 14:34:35 -0800

    add combinations

--- Reverse Array Recursively/reverseArr.js
+++ Reverse Array Recursively/reverseArr.js
@@ -1,12 +1,12 @@
 //reverse an array recursively
 //input = [0,1,2,3,4,5]
 //output = [5,4,3,2,1,0]
-let input = [0, 1, 2, 3, 4, 5];
+let input = [0, 1, 2, 3, 4, 5, 6];
 
 function reverseArray(array) {
 
     function helper(arr, i) {
-        if (arr.length / 2 < i) return arr;
+        if (arr.length / 2 <= i) return;
 
         let temp = arr[i];
         arr[i] = arr[arr.length - 1 - i];
